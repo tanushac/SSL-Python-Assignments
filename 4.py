72ff958bf4169681fec361ff3ea0e85a12131fcc
Tanusha Choudhary <144540796+tanushac@users.noreply.github.com>
2024-02-05 11:30:44 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,7 @@
 """ Created by Tanusha Choudhary
-Python program to convert binary, octal, and hexadecimal numbers to integers and displaying the results. This program defines lists of binary, octal, and hexadecimal numbers. It then converts these numbers to integers using the int() function with base 2 for binary, base 8 for octal, and base 16 for hexadecimal. Finally, it prints the resulting integer values. """ 
+Python program to convert binary, octal, and hexadecimal numbers to integers and displaying the results.
+This program defines lists of binary, octal, and hexadecimal numbers. It then converts these numbers to integers using the int() function with base 2 for binary,
+base 8 for octal, and base 16 for hexadecimal. Finally, it prints the resulting integer values. """ 
 
 binary = ["1100", "101", "11001", "1001", "110"] 
 intbinary = [int(binary, 2) for binary in binary] 
